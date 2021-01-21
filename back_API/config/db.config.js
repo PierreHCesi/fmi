fef25fdfff31542040e7e119252c1dc98e37e080
PierreHCesi <68538683+PierreHCesi@users.noreply.github.com>
2021-01-21 11:14:22 +0100

    Update db.config.js

--- back_API/config/db.config.js
+++ back_API/config/db.config.js
@@ -1,21 +1,7 @@
-// module.exports = {
-//     HOST: "localhost",
-//     USER: "root",
-//     PASSWORD: "",
-//     DB: "fmi",
-//     dialect: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         acquire: 30000,
-//         idle: 10000
-//     }
-// };
-
 module.exports = {
-    HOST: "bdd.madevol.ovh",
-    USER: "fmi",
-    PASSWORD: "projetFmi",
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
     DB: "fmi",
     dialect: "mysql",
     pool: {
